5425544032ea5cd2951f164549301dbc520d2f7a
Janmejay1198 <janmejaymohanty28899@gmail.com>
2024-05-01 20:56:57 -0400

    Prompt Engineering

--- application.py
+++ application.py
@@ -23,14 +23,12 @@ st.subheader('Sleep dataset')
 st.write(df)
 
 user_input = st.text_input("Ask Questions", "")
-prompt =  "Make this: " + user_input + " prompt better"
-better_prompt = model.generate_content(user_input).text
-
-st.subheader('Gemini Pro')
 
 if user_input:  # Check if user input is not empty
 
-    input_prompt = "This is user asking : " + better_prompt + "\n\n based on what user asked, this is the sleep data:" + str(sleep_data) + "\n\n Give me short quantified answer in 1 to 5 lines only based on the sleep data: "
+    better_prompt = "Make this:" + user_input + " into this good question to be asked"
+    prompt = model.generate_content(better_prompt).text
+    input_prompt = "This is user asking : " + prompt + "\n\n based on what user asked, this is the sleep data:" + str(sleep_data) + "\n\n Give me short quantified answer in 1 to 5 lines only based on the sleep data: "
     response = model.generate_content(input_prompt).text
     st.write("Gemini Pro:\n", response)
 
