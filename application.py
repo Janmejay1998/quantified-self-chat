eb341163c20269066e84106a3d6b2d65f74a861a
Ami3466 <100313966+Ami3466@users.noreply.github.com>
2024-05-03 00:20:01 -0400

    Update application.py
    Returned to the first point.
    still there is a problem with the csv uploaded

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ def load_data():
     """Load and preprocess the data."""
     uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file.getvalue(), delimiter=',')
+        df = pd.read_csv(uploaded_file, delimiter=',')
         df.drop(columns=['Person ID', 'Gender', 'Age', 'Occupation', 'Nurse ID'], inplace=True)
         return df.to_dict(orient='list')
 
